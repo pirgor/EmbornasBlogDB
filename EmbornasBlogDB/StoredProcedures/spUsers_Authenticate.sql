2a5975321fc92f83f06d278b60864dbb07274ad8
Sean E. <sembornas@mymail.mapua.edu.ph>
2024-06-21 14:42:50 +0800

    Embornas LE3

---
+++ EmbornasBlogDB/StoredProcedures/spUsers_Authenticate.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_Authenticate]
+	@username nvarchar(16),
+	@password nvarchar(16)
+AS
+begin
+	set nocount on;
+	SELECT [Id], [UserName], [FirstName], [LastName], [Password]
+	from dbo.Users
+	WHERE UserName = @username
+	AND Password = @password;
+end
